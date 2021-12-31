149d3961f82cce8c618e6643bff6ea6534bf1413
RuFrontEnd <k718198@gmail.com>
2021-12-31 21:06:20 +0800

    解決範疇汙染問題

--- type.ts
+++ type.ts
@@ -1,3 +1,5 @@
+export {}; // 讓 ts 視每個 .ts 檔案都為一個 block scope 
+
 const today = new Date();
 today.getMonth();
 // today.abc; // 這邊會提示 abc 不包含在 Date 物件內
@@ -7,8 +9,6 @@ const person = {
 };
 // person.abc // err
 
-class Color {
-
-}
-const red = new Color()
+class Color {}
+const red = new Color();
 // red.abc // err
