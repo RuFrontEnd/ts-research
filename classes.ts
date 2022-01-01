2fb5b5550bcc0225b8cd7158252e8eac3f9b500e
RuFrontEnd <k718198@gmail.com>
2022-01-01 18:13:34 +0800

    補充 classes 各種用法

--- classes.ts
+++ classes.ts
@@ -1,12 +1,44 @@
-class Vehicle {
-  drive(): void {
-    console.log("chugga chugaa");
-  }
+export {};
 
-  honk(): void {
+// public => this method / variable can be called any where
+// private => this method / variable can only be called by other methods in this class
+// protected => this method / variable can be called by other methods in this class, or by other methods in child classes
+
+class Vehicle {
+  public color: string;
+
+  constructor(color) {
+    this.color = color;
+  } // constructor 接受參數來定義一個實例的屬性
+
+  protected honk(): void {
     console.log("beep");
   }
 }
 
-const vehicle = new Vehicle();
-vehicle.drive();
+const vehicle = new Vehicle(123);
+// vehicle.honk() // err
+console.log(vehicle.color);
+
+class Car extends Vehicle {
+  public wheels: number;
+
+  constructor(wheels, color: string) {
+    super(color);
+    this.wheels = wheels;
+    this.color = color;
+  } // constructor 接受參數來定義一個實例的屬性
+
+  private drive(): void {
+    console.log("vroom");
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
+}
+
+const car = new Car(4, "red");
+// car.drive(); // err 因為是私有方法
+car.startDrivingProcess();
