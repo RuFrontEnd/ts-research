7a29e5fd2fa4f40ac86ae90a246ad11015fb9374
RuFrontEnd <k718198@gmail.com>
2022-01-03 21:32:40 +0800

    新增任意屬性與唯獨屬性

--- basic/interfaces.ts
+++ basic/interfaces.ts
@@ -11,6 +11,42 @@ const oldCivic = {
   },
 };
 
+// 任意屬性
+interface AirPlane {
+  name: string;
+  speed: number;
+  [propName: string]: any;
+}
+
+const helicopter = {
+  name: "eagle-008",
+  speed: 300,
+  backup: "new one",
+};
+
+interface Monster {
+  name: string;
+  // height: number; // err => number 不是 string 的子集
+  [propName: string]: string;
+} // 任意屬性型別一定要是確定屬性型別的父級
+
+// 唯獨屬性
+interface Creature {
+  readonly id: number;
+  name: string;
+  isOviparous: boolean;
+  [propName: string]: any;
+} // 任意屬性型別一定要是其他型別的父級
+
+let maoMao: Creature = {
+  id: 1,
+  name: "Mao",
+  isOviparous: false,
+  backUp: "I am cute!",
+};
+
+// maoMao.id = 2 // err => id是唯獨的不可改變
+
 const drink = {
   color: "brown",
   carbonated: true,
