3fd770c6fb14ce1e4ba4db406bb143c41748f89a
Luke_Xie <Luke_Xie@wistron.com>
2022-01-04 16:58:45 +0800

    補充 type

--- basic/type.ts
+++ basic/type.ts
@@ -13,3 +13,18 @@ class Color {}
 const red = new Color();
 // red.abc // err
 
+// 型別別名
+type Name = string | number;
+let str: Name = "123"; // 限制為 Name 型別
+let number: Name = 123; // string 或 number 皆可
+// let trueOrFalse: Name = false; // err => false 不是 string 或 number
+
+// 字串字面量型別
+type EventNames = "click" | "scroll" | "mousemove";
+function handleEvent(event: EventNames) {
+  // do something
+}
+handleEvent("click");
+handleEvent("scroll");
+handleEvent("mousemove");
+// handleEvent("XXX"); // err => 字串不屬於 click / scroll / mousemove;
