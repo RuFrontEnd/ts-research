3e0863e145a25318607c2a8557c55456e7b03803
RuFrontEnd <k718198@gmail.com>
2022-01-04 20:43:21 +0800

    新增斷言與列舉

---
+++ basic/assertion.ts
@@ -0,0 +1,24 @@
+export {};
+// type assertion => 型別斷言
+// <型別>值
+// 值 as 型別 => tsx 必須用這個
+
+function getLength(something: string | number): number {
+  if ((<string>something).length) {
+    return (<string>something).length;
+  } else {
+    return something.toString().length;
+  }
+} // 條件式中只有當 something 為 string 時才可以使用 .length 屬性, 但因為 something 也有可能是 number, 所以就要使用型別斷言的方式指定
+
+getLength(18);
+
+// or
+
+function asGetLength(something: string | number): number {
+  if ((something as string).length) {
+    return (something as string).length;
+  } else {
+    return something.toString().length;
+  }
+}
---
+++ basic/enum.ts
@@ -0,0 +1,33 @@
+// 列舉 Enum 型別用於取值被限定在一定範圍內的場景
+
+enum Days {
+  Sun,
+  Mon,
+  Tue,
+  Wed,
+  Thu,
+  Fri,
+  Sat,
+}
+
+console.log(Days["Sun"] === 0); // true
+console.log(Days["Mon"] === 1); // true
+console.log(Days["Tue"] === 2); // true
+console.log(Days["Sat"] === 6); // true
+
+console.log(Days[0] === "Sun"); // true
+console.log(Days[1] === "Mon"); // true
+console.log(Days[2] === "Tue"); // true
+console.log(Days[6] === "Sat"); // true
+
+enum Season {
+  Winter = 4,
+  Spring = 1,
+  Summer,
+  Fall,
+}
+
+console.log(Season["Winter"] === 4); // true
+console.log(Season["Spring"] === 1); // true
+console.log(Season["Summer"] === 2); // true 未被賦職者依順序遞增
+console.log(Season["Fall"] === 3); // true 未被賦職者依順序遞增
