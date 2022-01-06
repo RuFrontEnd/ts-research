ecfe383a7640d1cfbb8e04447a45247ace479363
RuFrontEnd <k718198@gmail.com>
2022-01-06 21:16:02 +0800

    新增 declare.d.ts / declare.ts

---
+++ basic/declare.ts
@@ -0,0 +1,12 @@
+export {};
+let declaredStrVariable = "string"; // 型別在 declare.d 已被定義
+
+// let nameVariable = "myName"; // declare nameSpace 定義物件型別
+
+let nameVariable: typeof Variables.name = 'str'; // declare nameSpace 定義變數型別
+
+let namespaceVariable: Variables.NamespaceProps = {
+  propA: "string",
+  propB: 1,
+  propC: true,
+}; // declare nameSpace 定義物件型別
