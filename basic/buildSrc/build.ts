2495a9c6ed5c1e31d311eb96148c1d5236f54dfd
RuFrontEnd <k718198@gmail.com>
2022-02-04 20:40:18 +0800

    修正 tsconfig 註解

--- basic/buildSrc/build.ts
+++ basic/buildSrc/build.ts
@@ -1,6 +1,6 @@
 // 指令下 tsc 時, 會按照 tsconfig 的設定去做編譯成 js 檔的動作
-import { variableValue } from "./exports";
-import defaultVariableValue from "./exports";
+import { variableValue } from "./buildB";
+import defaultVariableValue from "./buildB";
 
 console.log("variableValue", variableValue);
 console.log("defaultVariableValue", defaultVariableValue);
--- basic/buildSrc/buildB.ts
+++ basic/buildSrc/buildB.ts
@@ -1,3 +1,4 @@
+// 提供 build.ts 需要的引入內容
 export const variableValue = "variableValue";
 
 const defaultVariableValue = "defaultVariableValue";
