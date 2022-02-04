13aaef42c646d6e8ee0f7d8864d9fc5d9e24bb2c
RuFrontEnd <k718198@gmail.com>
2022-02-04 20:49:02 +0800

    更新 declare.d.ts to declareB

--- basic/declare.ts
+++ basic/declare.ts
@@ -1,9 +1,9 @@
 export {};
-let declaredStrVariable = "string"; // 型別在 declare.d 已被定義
+let declaredStrVariable: declaredStrVariableType = "string"; // 型別在 declare.d 已被定義
 
 // let nameVariable = "myName"; // declare nameSpace 定義物件型別
 
-let nameVariable: typeof Variables.name = 'str'; // declare nameSpace 定義變數型別
+let nameVariable: Variables.name = "str"; // declare nameSpace 定義變數型別
 
 let namespaceVariable: Variables.NamespaceProps = {
   propA: "string",
--- basic/declareB.ts
+++ basic/declareB.ts
@@ -1,12 +1,12 @@
 // 宣告語句 declare
 // 檔名以 .d.ts 命名 => ts 會去解析型別檔案( 可供全域使用 )
 
-declare let declaredStrVariable: string;
+declare type declaredStrVariableType = string;
 
 // 命名空間
-declare namespace Variables {
-  const name: string;
-  interface NamespaceProps {
+namespace Variables {
+  export type name = string;
+  export interface NamespaceProps {
     propA: string;
     propB: number;
     propC: boolean;
