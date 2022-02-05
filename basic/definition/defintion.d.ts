b97ed012ac277af3874cbd3b76c1a9be45b39a62
RuFrontEnd <k718198@gmail.com>
2022-02-05 19:50:04 +0800

    新增 tsconfig 的 include 屬性

--- basic/definition/defintion.d.ts
+++ basic/definition/defintion.d.ts
@@ -1,3 +1,3 @@
 declare namespace DefinitionType {
-  const str: string;
+  type str = string;
 }
--- basic/definition/defintion.ts
+++ basic/definition/defintion.ts
@@ -1,1 +1,3 @@
+// tsconfig 加入 include 屬性才吃的到 .d.ts file
+
 const str: DefinitionType.str = "string";
